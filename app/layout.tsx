7ca15ae21e0b7fc1ad14a6244e7a488969728c58
Erik Olivero <erik.olivero@jobtome.com>
2024-08-21 10:00:11 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils"
-import Header from "@/components/header";
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 const fontSans = FontSans({
@@ -29,6 +30,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
