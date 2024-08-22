875e1e78bb123a74930fa6caed1d362254c94660
Erik Olivero <erik.olivero@jobtome.com>
2024-08-22 09:01:08 +0200

    Add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -29,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
